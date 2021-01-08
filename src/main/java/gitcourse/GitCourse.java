55374e826f5fbe55e4a387b444c81b0a809bb317
Andrei Tsyulia <andrei_tsyulia@epam.com>
2021-01-08 13:50:33 +0300

    Add main method

--- src/main/java/gitcourse/GitCourse.java
+++ src/main/java/gitcourse/GitCourse.java
@@ -1,6 +1,11 @@
 package gitcourse;
 
 public class GitCourse {
-    private String line ="dd";
-    private String line2 ="dd";
+    private static final String HELLO = "Hello";
+    private static final String WORLD = "world";
+
+    public static void main(String[] arg) {
+        System.out.println("Hello from git");
+        System.out.println(HELLO + WORLD);
+    }
 }
