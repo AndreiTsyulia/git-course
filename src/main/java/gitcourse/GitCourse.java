fcc4d91e8a73da2e091be74aac608921eab0e670
Andrei Tsyulia <andrei_tsyulia@epam.com>
2021-01-08 15:43:38 +0300

    Changed pom and GitCourse

--- src/main/java/gitcourse/GitCourse.java
+++ src/main/java/gitcourse/GitCourse.java
@@ -1,10 +1,18 @@
 package gitcourse;
 
+import org.testng.annotations.Test;
+
 public class GitCourse {
     private static final String HELLO = "Hello";
     private static final String WORLD = "world";
 
     public static void main(String[] arg) {
+        System.out.println(HELLO + " from git");
+        System.out.println(HELLO + WORLD);
+    }
+
+    @Test
+    public void test(){
         System.out.println(HELLO + " from git");
         System.out.println(HELLO + WORLD);
     }
